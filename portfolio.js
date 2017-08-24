eb459627fe57a8d30d05e77e9353f6820274b3ca
leeeunji <eunji1234@gmail.com>
2017-08-25 01:49:19 +0900

    170825-수정

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
 $(function(){
     //slide
     var idx = 0;
-        function movemuent(start,end,target){
+        function movement(start,end,target){
              $('.slide li').eq(target).css({
                 'left':start,
                 'display':'block'
@@ -9,26 +9,29 @@ $(function(){
                 'left':end
             },700)
         }
+        var slideInter = setInterval(function(){
+            $('.port_next').click();
+        },2500)
 
         $('.slide_arrow .port_next').click(function(){
-            movemuent('0','-100%',idx);
+            movement('0','-100%',idx);
             idx++
-            movemuent('100%','0',idx);
+            movement('100%','0',idx);
 
             if(idx ==$('.slide li').length){
                 idx = 0
-                 movemuent("100%","0",idx);
+                 movement("100%","0",idx);
             }
         })
         $('.slide_arrow .port_next').trigger('click')
 
        $('.slide_arrow .port_prev').click(function(){
-           movemuent('0','100%',idx);
+           movement('0','100%',idx);
            idx--
-           movemuent('-100%','0',idx);
+           movement('-100%','0',idx);
            if(idx < 0){
                idx == 4
-               movemuent('100%','0',idx);
+               movement('100%','0',idx);
            }
         })
 
