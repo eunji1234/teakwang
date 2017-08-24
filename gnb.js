22d6b0a913587930790f118fc9023e04e3913828
leeeunji <eunji1234@gmail.com>
2017-08-24 14:19:07 +0900

    170824

--- gnb.js
+++ gnb.js
@@ -5,4 +5,21 @@ $(function(){
         $('.menu_bg').stop().slideUp()
     })
 
+    // $('.hamburger').click(function(){
+    //     console.log('asdf');
+    //     if(true){
+    //         $('.nav_menu').stop().animate({
+    //         'right':'0'
+    //         })
+    //     }
+    //     else{
+    //         $('.wrap').click(function(){
+    //             $('.nav_menu').stop().animate({
+    //                 'right':'-240px'
+    //             })
+    //         })
+    //     }
+    // })
+
+
 })
